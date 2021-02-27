970427b54f1d0773fe59d4a022b7e02fd29fd1a0
Madison Ng <mtng2@dons.usfca.edu>
2021-02-26 17:36:59 -0800

    Add edited 01 script, archive old version

---
+++ code/01_subset_data_to_state_for_loop.R
@@ -0,0 +1,33 @@
+# Script to read in Apple mobility data from a csv file
+# and subset data to just include rows from any US state
+# and write out a csv file that only includes this subset
+# uses function in the code/functions directory
+
+# Madison Ng
+# mtng2@dons.usfca.edu
+# February 22, 2021
+
+# load functions
+source("code/functions/subset_mobility_data_to_state.R")
+
+# vector to pick any state to subset
+pick_any_state_vector <- c("Maine",
+                           "Virginia",
+                           "Wisconsin",
+                           "Hawaii",
+                           "Arizona")
+
+# use of function via for loop
+for (state_to_subset in pick_any_state_vector) {
+
+  x <- state_to_subset
+
+  print(paste("The state you chose is", state_to_subset))
+  state_to_subset <- subset_mobility_data_to_state(
+    input_file_name = "data/raw_data/applemobilitytrends-2021-02-22.csv",
+    state_to_subset = x)
+
+  print(paste("Subsetting data for", x))
+  print("State data now subsetted.")
+  print("--------")
+}

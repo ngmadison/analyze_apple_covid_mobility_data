aa62596062690a2ee3351368b73890ed95503695
Madison Ng <mtng2@dons.usfca.edu>
2021-03-29 01:28:49 -0700

    Add another state and data set to bash script

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -2,4 +2,6 @@
 
 # Bash script for rendering Rmarkdown file using parameters passed in from shell
 
-Rscript -e "rmarkdown::render('analysis.Rmd')"
+Rscript -e "rmarkdown::render('analysis.Rmd', \
+            params = list(state = 'Wyoming',\
+            data = 'data/raw_data/applemobilitytrends-2021-03-06.csv'))"

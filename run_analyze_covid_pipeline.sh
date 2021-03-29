4ea80bb1c723afdf4faccb8b560c6d6cf59da58d
Madison Ng <mtng2@dons.usfca.edu>
2021-03-29 03:36:29 -0700

    Update if statement

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -14,7 +14,7 @@ if [ $# -eq 0 ]
 then
   echo "Supply three arguments to run this script:"
   echo "First, pick a US state to analyze."
-  echo "Second select a city in that state."
+  echo "Second select a city in that state.Reference data for correct names."
   echo "Finally include the path to the mobility data csv."
   exit 1
 fi
